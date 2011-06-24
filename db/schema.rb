762510e9ee850fffeea1935f3976843c2ac667b4
Mateusz <g.r3z0n@vp.pl>
2011-06-24 15:00:51 +0200

    problem scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110624125900) do
+ActiveRecord::Schema.define(:version => 20110624130022) do
+
+  create_table "problems", :force => true do |t|
+    t.integer  "project_id"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "name"
